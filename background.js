4f07e5e3a583acbc6c25043eca718b4e236e49b4
HJK-G <34551787+HJK-G@users.noreply.github.com>
2021-07-27 22:21:18 -0700

    done

--- background.js
+++ background.js
@@ -1,35 +1,35 @@
-var popupId;
+var popupId
+
+function openPopup() {
+  chrome.windows.create(
+    {
+      url: 'popup.html',
+      type: 'popup',
+      focused: true,
+      width: 300,
+      height: 500,
+    },
+    function (popup) {
+      popupId = popup.id
+    },
+  )
+}
 
 chrome.action.onClicked.addListener(function (tab) {
   console.log('Using Image Paster')
 
-  // If popupId is undefined then there isn't a popup currently open.
   if (typeof popupId === 'undefined') {
-    // Open the popup
-    chrome.windows.create(
-      {
-        url: 'popup.html',
-        type: 'popup',
-        focused: true,
-        width: 350,
-        height: 520,
-      },
-      function (popup) {
-        popupId = popup.id
-      },
-    )
-  } // There's currently a popup open
-  else {
-    // Bring it to the front so the user can see it
-    chrome.windows.update(popupId, { focused: true })
+    openPopup()
+  } else {
+    chrome.windows.remove(popupId)
+
+    openPopup()
   }
 })
 
 // When a window is closed
-chrome.windows.onRemoved.addListener(function(windowId) {
-  // If the window getting closed is the popup we created
+chrome.windows.onRemoved.addListener(function (windowId) {
   if (windowId === popupId) {
-    // Set popupId to undefined so we know the popups not open
-    popupId = undefined;
+    popupId = undefined
   }
-});
+})
--- js/popup.js
+++ js/popup.js
@@ -1,28 +1,92 @@
-let imgurl = document.getElementById('imgurl')
+let clientid = 'f1a30925998c04a'
+let endpoint = 'https://api.imgur.com/3/image'
 
-// image should already be copied, paste will open the popup with the clipboard contents
+let imgurl = document.getElementById('imgurl')
 let pastedImage = document.getElementById('pastedImage')
+let resultText = document.getElementById('result')
+
+function post(path, data, callback) {
+  var xhttp = new XMLHttpRequest()
+
+  xhttp.open('POST', path, true)
+  xhttp.setRequestHeader('Authorization', 'Client-ID ' + clientid)
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status >= 200 && this.status < 300) {
+        var response = ''
+        try {
+          response = JSON.parse(this.responseText)
+        } catch (err) {
+          response = this.responseText
+        }
+        callback.call(window, response)
+      } else {
+        console.log(this.responseText)
+        throw new Error(this.status + ' - ' + this.statusText)
+      }
+    }
+  }
+  xhttp.send(data)
+  xhttp = null
+}
+
+function copyURL(link) {
+  navigator.permissions.query({ name: 'clipboard-write' }).then((result) => {
+    if (result.state == 'granted' || result.state == 'prompt') {
+      navigator.clipboard.writeText(link).then(
+        () => {
+          console.log('Wrote URL to clipboard')
+        },
+        () => {
+          console.log('Clipboard write failed')
+        },
+      )
+    }
+  })
+}
+
+var feedback = function (res) {
+  if (res.success === true) {
+    var link = res.data.link.replace(/^http:\/\//i, 'https://')
+    console.log(link)
+
+    imgurl.href = link
+    imgurl.textContent = link
+
+    resultText.textContent =
+      'Link has been copied to clipboard. This window will close in 3 seconds.'
+
+    copyURL(link)
+
+    window.setTimeout(() => {
+      window.close()
+    }, 3000)
+  }
+}
 
 navigator.permissions.query({ name: 'clipboard-read' }).then((result) => {
-  // If permission to read the clipboard is granted or if the user will
-  // be prompted to allow it, we proceed.
-  console.log(result.state);
-
   if (result.state == 'granted' || result.state == 'prompt') {
     navigator.clipboard.read().then((data) => {
       for (let i = 0; i < data.length; i++) {
         if (!data[i].types.includes('image/png')) {
-          console.log('Clipboard contains non-image data. Unable to access it.');
+          console.log('Clipboard contains non-image data. Unable to access it.')
         } else {
           data[i].getType('image/png').then((blob) => {
             console.log('Got an image')
-            imgElem.src = URL.createObjectURL(blob);
+            pastedImage.src = URL.createObjectURL(blob)
+
+            console.log(blob)
+            var fd = new FormData()
+            fd.append('image', blob)
+
+            post(endpoint, fd, function (data) {
+              console.log('posted')
+              feedback(data)
+            })
           })
         }
       }
-
-      console.log('dATA')
     })
-    console.log('IDK')
   }
 })
