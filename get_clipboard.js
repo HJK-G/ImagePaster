6b7e2307031d5b4af75872e69c45c0038196a44b
HJK-G <34551787+HJK-G@users.noreply.github.com>
2021-07-28 00:13:36 -0700

    remove extra unnecessary

--- get_clipboard.js
+++
@@ -1,9 +0,0 @@
-navigator.clipboard
-  .read()
-  .then((res) => {
-    chrome.runtime.sendMessage({
-      message: 'copied',
-      payload: `"${res}"`,
-    })
-  })
-  .catch((err) => console.log(err))
